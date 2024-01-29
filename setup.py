b3a2f7be3b78825240717157cecaaaed8d285fae
crisly <108693780+crislymss@users.noreply.github.com>
2024-01-29 19:23:27 -0300

    adicionando e removendo arquivos

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2"
+VERSION = "1.0.0"
 DESCRIPTION = "Pacote de teste"
-LONG_DESCRIPTION = "Biblioteca Python para manipulações de serviços de contabilidade."
+with open("README.md", "r", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
+#LONG_DESCRIPTION = "Biblioteca Python para manipulações de serviços de contabilidade."
 
 
 REQUIRED_PACKAGES = [
@@ -13,15 +15,15 @@ REQUIRED_PACKAGES = [
 
 setup(
     # the name must match the folder name
-    name="Contablibi",
+    name="poocontab",
     version=VERSION,
-    author="crisly, erik",
-    author_email="eriklustosa8@gmail.com",
+    author="crisly",
+    author_email="crislymaria21@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     install_requires=REQUIRED_PACKAGES,  # adiciona bibliotecas adicionais
-    keywords=['python', 'contablib'],
+    keywords=['python', 'cont'],
     classifiers=["Development Status :: 3 - Alpha",]
 
 )
